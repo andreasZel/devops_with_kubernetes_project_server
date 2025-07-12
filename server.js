7d07c24ec887d34faf2eaa45aaae409b8c188571
andreas <antreaszel@gmail.com>
2025-07-12 17:38:54 +0300

    serve static web page

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const { PORT } = process.env;
 const port = PORT ?? 4000;
 
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen((port), () => {
     console.log(`Server started in port ${port}`)
