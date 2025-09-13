d84628f40cfa8167559859736db99e80df211d8b
andreas <antreaszel@gmail.com>
2025-09-13 16:05:53 +0300

    fixes to use env vars and postgres login

--- todoApp/backup/backup.sh
+++ todoApp/backup/backup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Construct URL if Postgres secrets are present
+if [ -n "${POSTGRES_USER:-}" ] && [ -n "${POSTGRES_PASSWORD:-}" ] && [ -n "${POSTGRES_HOST:-}" ] && [ -n "${POSTGRES_PORT:-}" ] && [ -n "${POSTGRES_DB:-}" ]; then
+    export URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+fi
+
 if [ -n "${URL:-}" ]; then
   DUMP_FILE="/usr/src/app/backup.sql"
   echo "Creating database dump..."
