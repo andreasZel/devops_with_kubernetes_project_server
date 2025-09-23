4421687925f9b28c5d41037965804ce3ef022c17
andreas <antreaszel@gmail.com>
2025-09-23 22:50:56 +0300

    test

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ async function saveImage(currentTime) {
 
 var app = express();
 
-app.use('/image', express.static(IMAGE_DIR_REL));
+app.use('/data/images', express.static(IMAGE_DIR_REL));
 
 app.use(express.static(path.join(__dirname, STATIC_DIR_REL)));
 
